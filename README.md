effbac0144cc254628f20dbeca03dd4ddeab0e54
MASAKI Haruka <yek@reasonset.net>
2020-07-30 22:37:26 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # atnow
+
 Store and view you own murmur.
+
+# Dependency
+
+* Ruby
+* Yad
+* Zenity
+* `xdg-user-dir`
+
+# Usage
+
+1. Start `atnow.rb`
+2. Icon will be in systray.
+3. Click icon.
+4. Murmuring.
+5. Right click icon.
+6. Click long murmur.
+
+# Advanced
+
+* You can open murmuring window with HUPping this process.
+* You can find this process's pid in `/var/run/atnow-$uid.pid` file.
---
+++ atnow.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/ruby
+
+rp, wp = IO.pipe
+
+PPID=$$
+DOC_DIR = `xdg-user-dir DOCUMENTS`.chomp
+
+Dir.mkdir "#{DOC_DIR}/atnow" unless File.exist? "#{DOC_DIR}/atnow"
+File.open("#{DOC_DIR}/atnow/now", "w") {|f| nil } unless File.exist? "#{DOC_DIR}/atnow/now"
+
+update_menu = ->() {
+  lines = File.readlines "#{DOC_DIR}/atnow/now"
+  if lines.length > 520
+    File.open("#{DOC_DIR}/atnow/archive-#{Time.now.strftime("%Y%m%d-%H")}", "w") do |f|
+      f.puts lines.first(500)
+    end
+    lines = lines[500..]
+    File.open("#{DOC_DIR}/atnow/now" ,"w") do |f|
+      begin
+        f.flock(File::LOCK_EX)
+        f.puts lines
+      ensure
+        f.flock(File::LOCK_UN)
+      end
+    end
+  end
+  wp.puts("menu:" + lines.last(20).map {|i| fs = i.strip.sub(/^.*?\t/, "")
+  fs.length > 64 ? sprintf('%s…`zenity --info --text="%s"`user-available', fs[0, 63], fs) : sprintf('%s``user-available', fs) }.join("|"))
+}
+
+update_menu.()
+
+pid = fork do
+  wp.close
+  STDIN.reopen(rp)
+  exec("yad", "--notification", "--listen", '--image=user-available', '--text="@Now"', '--item-separator=`', "--command=kill -HUP #{PPID}")
+end
+
+rp.close
+
+Signal.trap(:HUP) do
+  text=`yad --entry --title="Your Tweet" --text="What's Happen?" --image=gtk-dialog-question --width=450`
+  if text.length > 0
+    File.open("#{DOC_DIR}/atnow/now" ,"a") do |f|
+      begin
+        f.flock(File::LOCK_EX)
+        f.puts([Time.now.strftime("%Y-%m-%d\t%H:%M:%S"), text.tr('|`', "/'")].join("\t"))
+      ensure
+        f.flock(File::LOCK_UN)
+      end
+    end
+    update_menu.()
+  end
+end
+
+begin
+  File.open("/var/run/atnow-#{Process:UID.eid}.pid", "w") {|f| f.print PPID}
+  Process.waitpid(pid)
+ensure
+  File.delete("/var/run/atnow-#{Process:UID.eid}.pid")
+end
